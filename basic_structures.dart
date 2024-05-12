8b92750f01a6a1248baf7a4a884cebfe3a5de6ca
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-12 16:35:06 +0000

    lists

--- basic_structures.dart
+++ basic_structures.dart
@@ -1,3 +1,8 @@
 void main() {
+  //creating a list literal using a comma-separated list of values
+  //surrouded with sqaure brackets
   final people = ['Pablo', 'Manda', 'Megan'];
+  people[0];
+  people[1];
+  people[2];
 }
