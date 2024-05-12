c8dc987538c42be5a6dfa1f5d8bd7df386279339
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-12 16:57:56 +0000

    map done

--- basis structures/map.dart
+++ basis structures/map.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  //Maps
+  final score = {'Eric': 9, 'Mark': 12, 'Wayne': 1};
+  //adding a new entry
+  score['Andrew'] = 0;
+  print(score);
+
+  final hashMap = HashMap.of(score);
+  print(hashMap);
+}
