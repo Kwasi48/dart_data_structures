8b8ff6c1606b26560910ffd2c3d0bb90aa64386b
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-12 15:36:48 +0000

    comparing algorithms

--- complexities.dart
+++ complexities.dart
@@ -7,5 +7,18 @@ void main() {
     return sum;
   }
 
-  print(sumFromOneTo(10000));
+//Frederick Gauss
+  int betterSumFromOneTo(int n) {
+    return n * (n + 1) ~/ 2;
+  }
+
+  //print(sumFromOneTo(10000));
+
+  //checking time required to compute the code
+  final start = DateTime.now();
+  final sum = betterSumFromOneTo(9000000000000000000);
+  final end = DateTime.now();
+  final time = end.difference(start);
+  print(sum);
+  print(time);
 }
