c3472e1446b11b14d86369b4d5d61e31378a4031
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-12 17:16:09 +0000

    checking duplicates

--- basis structures/set.dart
+++ basis structures/set.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  var bag = {'Candy', 'Juice', 'Gummy'};
+  bag.add('Candy');
+  print(bag);
+
+  //used for finding duplicates
+  final myList = [1, 2, 2, 3, 4];
+  final mySet = <int>{};
+  for (final item in myList) {
+    if (mySet.contains(item)) {}
+    mySet.add(item);
+  }
+}
