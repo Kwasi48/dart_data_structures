c8f5cd097b68b80c6a7716426f924c621915e24b
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-16 02:36:39 +0000

    added push method

--- linked list/linked_list.dart
+++ linked list/linked_list.dart
@@ -24,4 +24,9 @@ class LinkedList<E> {
     if (isEmpty) return 'Empty list';
     return head.toString();
   }
+
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
 }
