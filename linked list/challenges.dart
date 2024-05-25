3c1554367240aa02318b86b1cb81c7b303d85a3a
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-25 14:35:31 +0000

    challenge 2 solved

--- linked list/challenges.dart
+++ linked list/challenges.dart
@@ -2,12 +2,14 @@ import 'linked_list.dart';
 
 void main() {
   final linq = LinkedList<int>();
+  linq.push(1);
+  linq.push(2);
   linq.push(3);
-  linq.push(4);
-  linq.push(5);
+  //linq.push(4);
   print(linq);
   print("--------------------------------------------------");
 
+//challenge 1
   void reverseNodes(LinkedList<int> list) {
     final internal = LinkedList<int>();
 
@@ -18,7 +20,18 @@ void main() {
     for (var element in internal) {
       print(element);
     }
+    //print("--------------------------------------------------");
+    //print(internal.length);
+  }
+
+  //Challenge2
+  void retMiddle(LinkedList<int> list) {
+    double middle = (list.length - 1) / 2;
+    print(middle);
+    print(' middle is ${list.nodeAt(middle.toInt())?.value}');
+    print(list);
   }
 
   reverseNodes(linq);
+  retMiddle(linq);
 }
