5a53a6455de6e0b016e3dfc661be17d4cb5daa60
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-25 12:53:46 +0000

    added some variable for the text

--- linked list/challenges.dart
+++ linked list/challenges.dart
@@ -1,6 +1,13 @@
 import 'linked_list.dart';
 
 void main() {
+  final linq = LinkedList<int>();
+  linq.push(3);
+  linq.push(4);
+  linq.push(5);
+  print(linq);
+  print("--------------------------------------------------");
+
   LinkedList<int> reverseNodes(LinkedList<int> list) {
     return list;
   }
