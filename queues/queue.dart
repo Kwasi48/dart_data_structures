d704de1b3e8d37ec7d128eef524037cc1fcfe69e
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-25 17:56:38 +0000

    adding a list in the queue

--- queues/queue.dart
+++ queues/queue.dart
@@ -4,3 +4,27 @@ abstract class Queue<E> {
   bool get isEmpty;
   E? get peek;
 }
+
+class QueueList<E> implements Queue<E> {
+  final _list = <E>[];
+
+  @override
+  dequeue() {
+    // TODO: implement dequeue
+    throw UnimplementedError();
+  }
+
+  @override
+  bool enqueue(element) {
+    // TODO: implement enqueue
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement isEmpty
+  bool get isEmpty => throw UnimplementedError();
+
+  @override
+  // TODO: implement peek
+  get peek => throw UnimplementedError();
+}
