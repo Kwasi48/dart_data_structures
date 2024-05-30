b005de994bae8a998cfc5fa3e47bd02636fb4981
Kwasi48 <abuwestwood@gmail.com>
2024-05-30 13:55:23 +0000

    solved challenge 1

--- queues/challenges.dart
+++ queues/challenges.dart
@@ -1,3 +1,13 @@
 void main(){
-  
+
+//challenge 1 Stack vs. Queue
+// a Stack has all the elements placed on top of each other and 
+//elements can be removed or added from the top whiles queue
+// have elements added from the bottom and removed from the front.
+//Real World Examples
+//Stack: a gun magazine 
+//Queue: Car assembly plants
+
+//challenge two
+
 }
