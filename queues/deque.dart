4cfbf3943fb3a0a3dc70c46bcf7c0cb4a20fd2cb
Kwasi48 <abuwestwood@gmail.com>
2024-05-30 21:48:22 +0000

    dequeue written

--- queues/deque.dart
+++ queues/deque.dart
@@ -10,3 +10,32 @@ abstract class Deque<E>{
   E? dequeue (Direction from);
 }
 
+class StackDequeue<E> implements Deque<E> {
+
+  final List _lizz = <E>[]; 
+  @override
+  E? dequeue(Direction from) {
+     if (from == Direction.front){
+      return _lizz.removeAt(0);
+     }  else if (from == Direction.back){
+        return _lizz.removeLast();
+     }
+  }
+
+  @override
+  bool enqueue(E element, Direction to) {
+    // TODO: implement enqueue
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement isEmpty
+  bool get isEmpty => throw UnimplementedError();
+
+  @override
+  E? peek(Direction from) {
+    // TODO: implement peek
+    throw UnimplementedError();
+  }
+
+}
