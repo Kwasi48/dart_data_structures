9354f5aa57736adb0e810de8e58b811d323a0690
Kwasi48 <abuwestwood@gmail.com>
2024-05-31 22:47:14 +0000

    a little refactoring

--- queues/queue.dart
+++ queues/queue.dart
@@ -1,3 +1,4 @@
+import "deque.dart";
 import "doubly_linked_list.dart";
 import "ring_buffer.dart";
 
@@ -116,6 +117,47 @@ class QueueStack<E> implements Queue<E>{
       ..._rightStack,
     ].join(',');
     return '[$combined]';
+  }
+
+}
+
+class StackDequeue<E> implements Deque<E> {
+
+  final List _lizz = <E>[]; 
+  @override
+  E? dequeue(Direction from) {
+     if (from == Direction.front){
+      return _lizz.removeAt(0);
+     }  else if (from == Direction.back){
+        return _lizz.removeLast();
+     }
+  }
+
+  @override
+  bool enqueue(E element, Direction to) {
+     if (to == Direction.back) {
+       _lizz.add(element);
+       return true;
+     } 
+      if (to == Direction.front) {
+        _lizz.insert(0, element);
+        return true;
+      }
+     return false; 
+     
+  }
+
+  @override
+  bool get isEmpty => _lizz.isEmpty;
+
+  @override
+  E? peek(Direction from) {
+     if (from == Direction.front){
+      return _lizz.first;
+     }
+     if (from ==  Direction.back){
+      return _lizz.last;
+     }
   }
 
 }
