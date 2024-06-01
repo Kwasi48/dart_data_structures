17308b08d9afa6dd17ad6621f59e3517fddb85d9
Kwasi48 <abuwestwood@gmail.com>
2024-06-01 21:19:26 +0000

    print depthfirst

--- trees/treemain.dart
+++ trees/treemain.dart
@@ -1,3 +1,5 @@
+ 
+
 import 'tree.dart';
 
 void main(){
@@ -6,4 +8,42 @@ void main(){
   final cold = TreeNode('Cold');
   beverages.add(hot);
   beverages.add(cold);
+final tree = makeBeverageTree();
+tree.forEachDepthFirst((node) => print(node.value));
+  
 }
+
+TreeNode<String> makeBeverageTree(){
+    final tree = TreeNode('beverages');
+    final hot = TreeNode('hot');
+    final cold = TreeNode('cold');
+    final tea = TreeNode('tea');
+    final coffee  = TreeNode('coffee');
+    final chocolate = TreeNode('cocoa');
+    final blackTea = TreeNode('black');
+    final greenTea = TreeNode('green');
+    final chaiTea = TreeNode('chai');
+    final soda = TreeNode('soda');
+    final milk = TreeNode('milk');
+    final gingerAle = TreeNode('ginger ale');
+    final bitterLemon = TreeNode('bitter lemon');
+
+    tree.add(hot);
+    tree.add(cold);
+
+    hot.add(tea);
+    hot.add(coffee);
+    hot.add(chocolate);
+
+    cold.add(soda);
+    cold.add(milk);
+
+    tea.add(blackTea);
+    tea.add(greenTea);
+    tea.add(chaiTea);
+
+    soda.add(gingerAle);
+    soda.add(bitterLemon);
+
+    return tree;
+  }
