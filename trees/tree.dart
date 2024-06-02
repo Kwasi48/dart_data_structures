5383d5302f0c53cd023cfc514efde5074f8de652
Kwasi48 <abuwestwood@gmail.com>
2024-06-02 13:10:13 +0000

    Search method using level order

--- trees/tree.dart
+++ trees/tree.dart
@@ -27,4 +27,14 @@ class TreeNode<T> {
       node = queue.dequeue();
     }
   }
+
+  TreeNode? Search (T value){
+    TreeNode? result;
+    forEachLevelOrder((node){
+      if (node.value == value){
+        result = node;
+      }
+    });
+    return result;
+  }
 }
