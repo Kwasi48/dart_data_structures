3118db6c58d7f6eb132720b44919fd959b5d7416
Kwasi48 <abuwestwood@gmail.com>
2024-06-02 13:28:17 +0000

    prelims

--- trees/challenges.dart
+++ trees/challenges.dart
@@ -1,3 +1,34 @@
+import 'tree.dart';
+
 void main(){
-  
+
+}
+
+
+TreeNode<int> levelNumbers(){
+  final fifteen = TreeNode(15);
+  final one = TreeNode(1);
+  final seventeen = TreeNode(17);
+  final twenty = TreeNode(20);
+  final secondone = TreeNode(1);
+  final five = TreeNode(5);
+  final zero = TreeNode(0);
+  final two = TreeNode(2);
+  final secondfive = TreeNode(5);
+  final seven = TreeNode(7);
+
+  fifteen.add(one);
+  fifteen.add(seventeen);
+  fifteen.add(twenty);
+
+  one.add(secondone);
+  one.add(five);
+  one.add(zero);
+
+  seventeen.add(two);
+
+  twenty.add(secondfive);
+  twenty.add(seven);
+
+  return fifteen; 
 }
