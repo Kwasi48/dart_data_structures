129af4352372257eeec49486084e6711d38cc485
Kwasi48 <abuwestwood@gmail.com>
2024-06-21 03:34:39 +0000

    inorder

--- Binary Trees/binary_node.dart
+++ Binary Trees/binary_node.dart
@@ -1,41 +1,48 @@
 class BinaryNode<T> {
   BinaryNode(this.value);
   T value;
-  BinaryNode<T> ? leftChild;
-  BinaryNode<T> ? rightChild;
+  BinaryNode<T>? leftChild;
+  BinaryNode<T>? rightChild;
 
-  @override 
-  String toString(){
+  @override
+  String toString() {
     return _diagram(this);
   }
 
-  String _diagram(BinaryNode<T>? node, [
+  String _diagram(
+    BinaryNode<T>? node, [
     String top = '',
     String root = '',
     String bottom = '',
-  ]){
-    if (node == null){
+  ]) {
+    if (node == null) {
       return '$root null\n';
     }
-    if (node.leftChild == null && node.rightChild == null ){
+    if (node.leftChild == null && node.rightChild == null) {
       return '$root ${node.value}\n';
     }
 
     final a = _diagram(
       node.rightChild,
-      '$top ', 
-'$top┌──', 
-'$top│ ', 
-  ); 
-final b = '$root${node.value}\n';
-     final c = _diagram( 
-    node.leftChild, 
-'$bottom│ ', 
-'$bottom└──', 
-'$bottom ', 
-  ); 
-return '$a$b$c'; 
-    
-    
+      '$top ',
+      '$top┌──',
+      '$top│ ',
+    );
+    final b = '$root${node.value}\n';
+    final c = _diagram(
+      node.leftChild,
+      '$bottom│ ',
+      '$bottom└──',
+      '$bottom ',
+    );
+    return '$a$b$c';
+  }
+
+  void traverseInOrder(void Function(T value) action){
+    leftChild?.traverseInOrder(action);
+    action(value);
+    rightChild?.traverseInOrder(action);
   }
 }
+
+
